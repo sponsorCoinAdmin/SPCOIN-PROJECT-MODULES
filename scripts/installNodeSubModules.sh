8adb411231b8b05e03d5149ac1e4b6d53b20875e
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-13 04:11:15 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,12 +1,23 @@
-echo ===== START INSTALLING NPM MODULES in $(PWD) =====
-echo START INSTALLING NPM Modules in $(PWD)
-CURR_DIR=$(PWD)
-echo START INSTALLING NPM Modules in $SPCOIN_BE_PATH
+exeIfDirExists() {
+    DIRECTORY=$1
+    COMMAND=$2
+    echo "FROM ROOT DIR        : $PWD"
+    if [ -d "$DIRECTORY" ]; then
+        echo "INSTALLING MODULES IN: $DIRECTORY"
+        echo "EXECUTING            : $COMMAND"
+    else 
+        echo "***ERROR: DIRECTORY $DIRECTORY not found"
+        echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
+    fi
+}
+
+exeIfDirExists $SPCOIN_BE_PATH "npm i"
 cd $SPCOIN_BE_PATH
-npm i 
-echo START INSTALLING NPM Modules in $SPCOIN_FE_PATH
+npm i
+cd ..
+exeIfDirExists $SPCOIN_BE_PATH "npm i"
 cd $SPCOIN_FE_PATH
 npm i 
-cd $CURR_DIR
+cd ..
 echo FINISHED INSTALLING NPM Modules in $(PWD)
 echo ===== FINISHED INSTALLING NPM MODULES in $(PWD) =====
