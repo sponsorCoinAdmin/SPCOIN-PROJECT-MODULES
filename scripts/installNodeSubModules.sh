f2708ba06b2965bbac085cc67bd6f3b18be6fb55
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-14 15:10:37 -0500

    Update installNodeSubModules.sh

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,35 +1,26 @@
 exeIfDirExists() {
     DIRECTORY=$1
     COMMAND=$2
-    echo "FROM ROOT DIR        : $PWD"
+    local CURR_DIR=$PWD
+    echo "CURRENT DIRECTORY : $PWD"
     if [ -d "$DIRECTORY" ]; then
         echo "INSTALLING MODULES IN: $DIRECTORY"
         echo "EXECUTING            : $COMMAND"
+        eval $COMMAND
     else 
-        echo "***ERROR: DIRECTORY $DIRECTORY not found"
+        echo "***ERROR: DIRECTORY $DIRECTORY NOT FOUND"
         echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
     fi
+    cd $CURR_DIR
 }
 
 clear
-echo HERE 1 $PWD
+echo ===== START INSTALLING SPONSORCOIN NPM MODULES in $(PWD) =====
+echo HERE 1
 exeIfDirExists $SPCOIN_BE_PATH "npm i"
-echo HERE 2 $PWD
-cd $SPCOIN_BE_PATH
-echo HERE 3 $PWD
-npm i
+echo HERE 2
+exeIfDirExists $SPCOIN_FE_PATH "npm i"
+echo HERE 3
+exeIfDirExists . "npm i"
 echo HERE 4 $PWD
-cd ..
-echo HERE 5 $PWD
-exeIfDirExists $SPCOIN_BE_PATH "npm i"
-echo HERE 6 $PWD
-cd $SPCOIN_FE_PATH
-echo HERE 7 $PWD
-npm i 
-echo HERE 8 $PWD
-cd ..
-echo HERE 9 $PWD
-echo FINISHED INSTALLING NPM Modules in $(PWD)
-echo HERE 10 $PWD
-echo ===== FINISHED INSTALLING NPM MODULES in $(PWD) =====
-echo HERE 11 $PWD
+echo ===== FINISHED INSTALLING SPONSORCOIN NPM MODULES in $(PWD) =====
