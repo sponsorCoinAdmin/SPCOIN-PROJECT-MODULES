d2f039597813f92db3e23972384be6d17ecf394b
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-16 01:56:59 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -4,23 +4,23 @@ exeIfDirExists() {
     local CURR_DIR=$PWD
     echo "CURRENT DIRECTORY : $PWD"
     if [ -d "$DIRECTORY" ]; then
-        echo "INSTALLING MODULES IN: $DIRECTORY"
-        echo "EXECUTING            : $COMMAND"
+        echo INSTALLING in: SUB-DIRECTORY $DIRECTORY =====
+        cd $DIRECTORY
+        echo "EXECUTING: $COMMAND"
         eval $COMMAND
     else 
-        echo "***ERROR: DIRECTORY $DIRECTORY NOT FOUND"
+        echo "***ERROR: SUB-DIRECTORY $DIRECTORY NOT FOUND"
         echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
     fi
     cd $CURR_DIR
 }
 
 clear
-echo ===== START INSTALLING SPONSORCOIN NPM MODULES in $(PWD) =====
+echo "===================== SPCOIN-PROJECT-MODULES ==============================="
+
 echo HERE 1
 exeIfDirExists $SPCOIN_BE_PATH "npm i"
 echo HERE 2
-exeIfDirExists $SPCOIN_FE_PATH "npm i"
-echo HERE 3
-exeIfDirExists . "npm i"
-echo HERE 4 $PWD
+# exeIfDirExists $SPCOIN_FE_PATH "npm i"
+# echo HERE 3
 echo ===== FINISHED INSTALLING SPONSORCOIN NPM MODULES in $(PWD) =====
