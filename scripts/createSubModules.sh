d57d1918097f730c8fe8ab4710c108a77d9af943
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-17 04:20:27 -0500

    WIP

---
+++ scripts/createSubModules.sh
@@ -0,0 +1,5 @@
+git submodule add git@github.com:sponsorCoinAdmin/spcoin-jmeter-tests.git
+git submodule add git@github.com:sponsorCoinAdmin/spcoin-docs.git
+git submodule add git@github.com:sponsorCoinAdmin/spcoin-nextjs-front-end.git
+git submodule add git@github.com:sponsorCoinAdmin/spcoin-hardhat-contract-access-test.git
+git submodule add git@github.com:sponsorCoinAdmin/node_modules-dev.git
