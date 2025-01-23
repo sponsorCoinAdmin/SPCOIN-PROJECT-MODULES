2b47ca22c775f3fcb453e4f6479ea3e3cdfbcc8e
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-23 01:30:43 -0500

    WIP

--- scripts/~home/spCoinSetup.sh
+++ scripts/~home/spCoinSetup.sh
@@ -1,22 +1,21 @@
 
-alias  setSpCoinEnv="export CLONE_PROJECT_LOG=CLONE_PROJECT.log ;
-                     export ACTIVE_PROJECT_NAME=SPCOIN-PROJECT-MODULES ;
-                     export ACTIVE_PROJECT_ROOT=\$PWD ;
-                     export ACTIVE_PROJECT_HOME=\$PWD/\$ACTIVE_PROJECT_NAME"
+alias initSpCoinEnv="export CLONE_PROJECT_LOG=CLONE_PROJECT.log ;
+              	     export ACTIVE_PROJECT_NAME=SPCOIN-PROJECT-MODULES ;
+	             export ACTIVE_PROJECT_ROOT=\$PWD ;
+	             export ACTIVE_PROJECT_HOME=\$PWD/\$ACTIVE_PROJECT_NAME"
 
 alias clearSpCoinEnv="export CLONE_PROJECT_LOG= ;
                       export ACTIVE_PROJECT_NAME= ;
-                      export ACTIVE_PROJECT_ROOT= ;
-                      export ACTIVE_PROJECT_HOME= "
+		      export ACTIVE_PROJECT_ROOT= ;
+		      export ACTIVE_PROJECT_HOME= "
 
 alias echoSpCoinEnv="echo CLONE_PROJECT_LOG=\$CLONE_PROJECT_LOG;
                      echo ACTIVE_PROJECT_NAME=\$ACTIVE_PROJECT_NAME ;
-                     echo ACTIVE_PROJECT_ROOT=\$ACTIVE_PROJECT_ROOT ;
-                     echo ACTIVE_PROJECT_HOME=\$ACTIVE_PROJECT_HOME "
+	             echo ACTIVE_PROJECT_ROOT=\$ACTIVE_PROJECT_ROOT ;
+	             echo ACTIVE_PROJECT_HOME=\$ACTIVE_PROJECT_HOME "
 
-alias cloneproj="clear;  setSpCoinEnv; cloneSponsorCoinProjects;"
+alias cloneproj="clear; cloneSponsorCoinProjects;"
 alias cdspc="cd ~/spcoin/\$SPCOIN-PROJECT-MODULES"
-
 
 installGitModules() {
     echo "############# INSTALL GIT MODULES ############"
@@ -45,21 +44,29 @@ installSecurityKeys() {
 
 installAliases() {
     echo ############## INSTALL PROJECecho ############T ALIASES ############"
-    cd $ACTIVE_PROJECT_HOME
+    echo HERE 1 current dir = $PWD
     echo "EXECUTING: . ./scripts/setupEnvAliases.sh";
+    echo HERE 2 current dir = $PWD
     . ./scripts/setupEnvAliases.sh;
+    echo HERE 3 current dir = $PWD
+    setroot
+    echo HERE 4 current dir = $PWD
+    cd $ACTIVE_PROJECT_HOME
+    echo HERE 5 current dir = $PWD
 }
 
 installNodeModules() {
     echo ############# INSTALL NODE MODULES ############"
     cd $ACTIVE_PROJECT_HOME
-    echo "EXECUTING: . ./scripts/installNodeSubModules.sh";
-    . ./scripts/installNodeSubModules.sh;
+    echo "EXECUTING: . ./scripts/installNodeModules.sh";
+    . ./scripts/installNodeModules.sh
     echo "EXECUTING: setroot";
+    setroot
 }
 
 cloneSponsorCoinProjects() {
-    installGitModules   2>&1 | tee    $CLONE_PROJECT_LOG
+    initSpCoinEnv       2>&1 | tee    $CLONE_PROJECT_LOG
+    installGitModules   2>&1 | tee -a $CLONE_PROJECT_LOG
     installSecurityKeys 2>&1 | tee -a $CLONE_PROJECT_LOG
     installAliases      2>&1 | tee -a $CLONE_PROJECT_LOG
     installNodeModules  2>&1 | tee -a $CLONE_PROJECT_LOG
